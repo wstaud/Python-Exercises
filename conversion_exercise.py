0d7ab399ba5815439b412a2d823bd5321d5536ab
unknown <xconstaud@gmail.com>
2017-06-13 09:47:25 -0500

    Add conversion exercise

---
+++ conversion_exercise.py
@@ -0,0 +1,20 @@
+"""Simple Conversion Exercise"""
+
+def prompt_user():
+    """Prompt User for input"""
+    user_input = input("Convert from:\n1: Decimal\n2: Int\n3: String\n4: Hex\n 5: Octal")
+
+    if user_input == '1':
+        return 1
+    elif user_input == '2':
+        return 1
+    elif user_input == '3':
+        return 1
+    elif user_input == '4':
+        return 1
+    elif user_input == '5':
+        return 1
+    else:
+        print('ERROR: Incorrect Input!')
+
+prompt_user()
